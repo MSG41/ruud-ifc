9e083b06d65c6da7be928b7d67f8e9f1eb5ead4a
Salim Thaifa <s.thaifa@amsterdam.nl>
2024-05-25 14:33:42 +0200

    vercel fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build', // Set the build output directory to 'build'
     rollupOptions: {
       // To treat 'three' as external (not included in the final bundle)
       external: ['three'],
-
-      // Alternatively, ensure it's included in your bundle if it's not being resolved
-      // This might be necessary if there's a specific way you need to handle 'three'
       output: {
+        // Define globals for externalized imports
         globals: {
-          three: 'THREE' // This is generally only necessary if you're treating it as external
-        }
-      }
-    }
-  }
+          three: 'THREE',
+        },
+      },
+    },
+  },
 });
