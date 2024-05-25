877844542313002cbd4a81a0f27ba2f41526fc17
Salim Thaifa <s.thaifa@amsterdam.nl>
2024-05-25 14:30:55 +0200

    vercel fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // To treat 'three' as external (not included in the final bundle)
+      external: ['three'],
+
+      // Alternatively, ensure it's included in your bundle if it's not being resolved
+      // This might be necessary if there's a specific way you need to handle 'three'
+      output: {
+        globals: {
+          three: 'THREE' // This is generally only necessary if you're treating it as external
+        }
+      }
+    }
+  }
+});
